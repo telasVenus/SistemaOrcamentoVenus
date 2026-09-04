48aa21e34f412c49b81e95adc58263ed915a2b33
Rhaissa Marques <rhaissa.marques@gmail.com>
2026-09-04 16:50:56 -0300

    update: cnpj and phone number

--- src/data/company.js
+++ src/data/company.js
@@ -2,8 +2,8 @@ export const company = {
   razaoSocial: 'JJR Produtos Siderúrgicos Ltda',
   nomeFantasia: 'Telas Vênus',
   endereco: 'Rua Marcolino Cruz, 81, Vila Santa Helena, Lagoa Santa/MG',
-  cnpj: '00.000.000/0001-00', // TODO: substituir pelo CNPJ real
-  telefone: '(31) 0000-0000', // TODO: substituir pelo telefone real
+  cnpj: '59.519.865/0001-01',
+  telefone: '(31) 98835-3453',
 };
 
 export const pickupAddress = company.endereco;
